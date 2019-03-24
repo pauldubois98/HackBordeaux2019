a0f478bd6f7d1c17340cd1d9bd4e2c1ed8e2b1f6
pauldubois98 <pauldubois98@gmail.com>
2019-03-24 03:48:42 +0100

    sorted serial problems

--- Arduino/lecture_cube/lecture_cube.ino
+++ Arduino/lecture_cube/lecture_cube.ino
@@ -33,132 +33,87 @@ void setup() {
   Serial.println("#####");
 }
 
+int i=0;
+
+
 void loop() {
-
-  digitalWrite(2,HIGH);
-  digitalWrite(3,LOW);
-  
-  a0 = analogRead(A0);
-  a0 = a0*5/1024;
-  r0 = (rref0*a0)/(5-a0);
-  
- 
-  a1 = analogRead(A1);
-  a1 = a1*5/1024;
-  r1 = (rref0*a1)/(5-a1);
-  
-  a2 = analogRead(A2);
-  a2 = a2*5/1024;
-  r2 = (rref1*a2)/(5-a2);
-  
-  a3 = analogRead(A3);
-  a3 = a3*5/1024;
-  r3 = (rref1*a3)/(5-a3);
-  
-  a4 = analogRead(A4);
-  a4 = a4*5/1024;
-  r4 = (rref2*a4)/(5-a4);
-  
-  a5 = analogRead(A5);
-  a5 = a5*5/1024;
-  r5 = (rref2*a5)/(5-a5);
-  
-  a6 = analogRead(A6);
-  a6 = a6*5/1024;
-  r6 = (rref3*a6)/(5-a6);
-  
-  a7 = analogRead(A7);
-  a7 = a7*5/1024;
-  r7 = (rref3*a7)/(5-a7);
-
-if ((Serial.available()>1)){
-  
-  if (Serial.read() == 'r') {
-    
-  Serial.print("[");
-  Serial.print(r0);
-  Serial.print(",");
-  Serial.print(r1);
-  Serial.print(",");
-  Serial.print(r2);
-  Serial.print(",");
-  Serial.print(r3);
-  Serial.print(",");
-  Serial.print(r4);
-  Serial.print(",");
-  Serial.print(r5);
-  Serial.print(",");
-  Serial.print(r6);
-  Serial.print(",");
-  Serial.print(r7);
-  Serial.println("]"); 
-
-  digitalWrite(2,LOW);
-  digitalWrite(3,HIGH);
-  delay(100);
-
-  a0 = analogRead(A0);
-  a0 = a0*5/1024;
-  r0 = (rref0*a0)/(5-a0);
-  
- 
-  a1 = analogRead(A1);
-  a1 = a1*5/1024;
-  r1 = (rref0*a1)/(5-a1);
-  
-  a2 = analogRead(A2);
-  a2 = a2*5/1024;
-  r2 = (rref1*a2)/(5-a2);
-  
-  a3 = analogRead(A3);
-  a3 = a3*5/1024;
-  r3 = (rref1*a3)/(5-a3);
-  
-  a4 = analogRead(A4);
-  a4 = a4*5/1024;
-  r4 = (rref2*a4)/(5-a4);
-  
-  a5 = analogRead(A5);
-  a5 = a5*5/1024;
-  r5 = (rref2*a5)/(5-a5);
-  
-  a6 = analogRead(A6);
-  a6 = a6*5/1024;
-  r6 = (rref3*a6)/(5-a6);
-  
-  a7 = analogRead(A7);
-  a7 = a7*5/1024;
-  r7 = (rref3*a7)/(5-a7);
-
-  Serial.print("[");
-  Serial.print(r0);
-  Serial.print(",");
-  Serial.print(r1);
-  Serial.print(",");
-  Serial.print(r2);
-  Serial.print(",");
-  Serial.print(r3);
-  Serial.print(",");
-  Serial.print(r4);
-  Serial.print(",");
-  Serial.print(r5);
-  Serial.print(",");
-  Serial.print(r6);
-  Serial.print(",");
-  Serial.print(r7);
-  Serial.println("]"); 
-
-
-  
+  if ((Serial.available()>0)){
+    char ch=Serial.read();
+    if (ch == 's'){
+      i=0;
+    }
+    if (ch == 'r') {
+      switch(i){
+        case 0:
+          digitalWrite(2,HIGH);
+          digitalWrite(3,LOW);
+          delay(100);
+          read();
+          i++;
+        break;
+        case 1:
+          digitalWrite(2,LOW);
+          digitalWrite(3,HIGH);
+          delay(100);
+          read();
+          i++;
+        break;
+        
+      }
+    }
   }
-
-  }
-
-  
-
 }
 
  
-    
-
-
+void read(){
+  a0 = analogRead(A0);
+  a0 = a0*5/1024;
+  r0 = (rref0*a0)/(5-a0);
+  
+  a1 = analogRead(A1);
+  a1 = a1*5/1024;
+  r1 = (rref0*a1)/(5-a1);
+  
+  a2 = analogRead(A2);
+  a2 = a2*5/1024;
+  r2 = (rref1*a2)/(5-a2);
+  
+  a3 = analogRead(A3);
+  a3 = a3*5/1024;
+  r3 = (rref1*a3)/(5-a3);
+  
+  a4 = analogRead(A4);
+  a4 = a4*5/1024;
+  r4 = (rref2*a4)/(5-a4);
+  
+  a5 = analogRead(A5);
+  a5 = a5*5/1024;
+  r5 = (rref2*a5)/(5-a5);
+  r5=i;
+  /*
+  a6 = analogRead(A6);
+  a6 = a6*5/1024;
+  r6 = (rref3*a6)/(5-a6);
+  
+  a7 = analogRead(A7);
+  a7 = a7*5/1024;
+  r7 = (rref3*a7)/(5-a7);
+  */
+  Serial.print("[");
+  Serial.print(r0);
+  Serial.print(",");
+  Serial.print(r1);
+  Serial.print(",");
+  Serial.print(r2);
+  Serial.print(",");
+  Serial.print(r3);
+  Serial.print(",");
+  Serial.print(r4);
+  Serial.print(",");
+  Serial.print(r5);
+  /*Serial.print(",");
+  Serial.print(r6);
+  Serial.print(",");
+  Serial.print(r7);
+  */Serial.println("]");
+}
